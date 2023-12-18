a4cf99ebc7ed1cc6a77f5dd2412fc460c06db158
Yakkinpizza <153895078+Yakkinpizza@users.noreply.github.com>
2023-12-18 22:26:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,55 @@
-from flask import Flask, render_template, request
-import math
+// Function to calculate the greatest common divisor (gcd)
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b);
+}
 
-app = Flask(__name__)
+function calculate() {
+    const coefficientsInput = document.getElementById('coefficientsInput').value;
 
-def calculate_values(a, b, c, d, p, n):
-    fnd = 0
-    if a * d - b * c == 0 or abs(d) - abs(b) != 1 or math.gcd(a * d - b * c, p) != 1:
-        return "ERROR", None
+    // Extract coefficients without spaces
+    const a = parseInt(coefficientsInput.charAt(0));
+    const b = parseInt(coefficientsInput.charAt(1));
+    const c = parseInt(coefficientsInput.charAt(2));
+    const d = parseInt(coefficientsInput.charAt(3));
 
-    X = ((((a * d - b * c) % p) ** (p - 2)) * ((d % p) + (-b % p))) % p
-    Y = ((((a * d - b * c) % p) ** (p - 2)) * ((-c % p) + (a % p))) % p
+    const modValue = parseInt(document.getElementById('modInput').value);
+    const powerValue = parseInt(document.getElementById('powerInput').value);
 
-    for i in range(int(math.log2(n))):
-        X1 = X * (2 + X * (c * b - a * d))
-        Y1 = Y - X * (d * a * Y - b * c * Y + c - a)
-        X = X1
-        Y = Y1
+    let result = '';
+    let foundError = false;
 
-    result_tuple = (
-    f"X = {X} + ({p}^{n}) * m) = {X} + ({p ** n}m)",
-    f"Y = {Y} + ({p}^{n}) * m) = {Y} + ({p ** n}m)"
-    )
+    // Check if a * d - b * c is zero or if the absolute difference is not 1 or if gcd(ad - bc, p) is not 1
+    if (a * d - b * c === 0 || Math.abs(d) - Math.abs(b) !== 1 || gcd(a * d - b * c, modValue) !== 1) {
+        result = 'ERROR';
+        foundError = true;
+    }
 
+    if (!foundError) {
+        let X = parseInt(((((a * d) - (b * c)) ** (modValue - 2)) * ((d % modValue) + (-b % modValue))) % modValue);
+        let Y = parseInt(((((a * d) - (b * c)) ** (modValue - 2)) * ((-c % modValue) + (a % modValue))) % modValue);
 
+        for (let i = 0; i < Math.log2(powerValue); i++) {
+            const X1 = parseInt(X * (2 + X * (c * b - a * d)));
+            const Y1 = parseInt(Y - X * (d * a * Y - b * c * Y + c - a));
+            X = X1;
+            Y = Y1;
+        }
 
-    return None, result_tuple
+        // Check if the final result of X or Y is more than P^n or less than zero
+        if (X < 0 || X >= Math.pow(modValue, powerValue) || Y < 0 || Y >= Math.pow(modValue, powerValue)) {
+            // Calculate new X and Y based on the specified format
+            const X1 = parseInt(X + Math.pow(modValue, powerValue) / Math.pow(modValue, powerValue) * Math.pow(modValue, powerValue));
+            const Y1 = parseInt(Y + Math.pow(modValue, powerValue) / Math.pow(modValue, powerValue) * Math.pow(modValue, powerValue));
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        a, b, c, d = map(int, request.form["coefficients"].split())
-        p = int(request.form["mod_value"])
-        n = int(request.form["power_value"])
+            // Calculate the output based on the specified format
+            const outputX = parseInt(X1 + Math.pow(modValue, powerValue) * Math.ceil(-X1 / Math.pow(modValue, powerValue)));
+            const outputY = parseInt(Y1 + Math.pow(modValue, powerValue) * Math.ceil(-Y1 / Math.pow(modValue, powerValue)));
 
-        error, results = calculate_values(a, b, c, d, p, n)
+            result = `Adjusted Result:\nX = ${outputX} + ${Math.pow(modValue, powerValue)}m\nY = ${outputY} + ${Math.pow(modValue, powerValue)}m`;
+        } else {
+            result = `Final Result:\nX = ${X} + ${Math.pow(modValue, powerValue)}m\nY = ${Y} + ${Math.pow(modValue, powerValue)}m`;
+        }
+    }
 
-        return render_template("index.html", error=error, results=results)
-    else:
-        return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    document.getElementById('result').innerText = result;
+}
